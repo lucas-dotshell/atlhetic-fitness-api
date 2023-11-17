7eca9ea40f800bb188838a5c25082e2f4e310300
Lucas Portugal <96060127+lucas-dotshell@users.noreply.github.com>
2023-11-17 00:43:45 -0300

    install jwt pkg

--- src/middlewares/autheticate.ts
+++ src/middlewares/autheticate.ts
@@ -19,7 +19,12 @@ export default {
                 errorDetails: { "pt-BR": "Forneça uma chave para autorização.", "en": "Provide a key to perform authorization." }
             });
         }
-
-
+        // JWT CASES
+        // aud - A token’s audience or recipient
+        // sub - A token’s subject, a unique identifier for whichever programmatic entity is referenced within the token(e.g., a user ID)
+        // iss - A token’s issuer ID
+        // iat - A token’s “issued at” time stamp
+        // nbf - A token’s “not before” time stamp; the token is rendered invalid before said time
+        // exp - A token’s “expiration” time stamp; the token is rendered invalid at said time
     }
 };
